b62d9d641bf4a7ea22d1a9f05404ee0010f374a5
Daniel Gohl <danielgohl13@gmail.com>
2020-06-11 22:18:14 -0400

    server initial setup

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+
+var corsOptions = {
+  origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
+
+
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.json({ message: "Aplicação de processos." });
+});
+
+const db = require("./app/models");
+db.sequelize.sync();
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server rodando na porta ${PORT}.`);
+});
+
+db.sequelize.sync({ force: true }).then(() => {
+    console.log("Drop and re-sync db.");
+  });
